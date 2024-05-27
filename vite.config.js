ad4eb4c643070c2ed7887543e087e4526a4c8a07
Christian <christdao19@gmail.com>
2024-05-27 11:37:35 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import PluginInspect from 'vite-plugin-inspect';
 
 export default defineConfig({
     plugins: [
+        PluginInspect(),
         laravel({
             input: [
-                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
