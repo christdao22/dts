b8efb2bfd9824aa8805f1f394a5f77b0f1dd24f4
Christian <christdao19@gmail.com>
2024-07-31 10:12:17 +0800

    remove register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -107,11 +107,11 @@
                                         In</button>
                                 </div>
                             </div>
-                            <div class="form-group mt-2 mb-0 row">
+                            {{-- <div class="form-group mt-2 mb-0 row">
                                 <div class="col-12 mt-3 text-center">
                                     <a href="{{ route('register') }}" class="text-muted">Register</a>
                                 </div>
-                            </div>
+                            </div> --}}
 
                         </form>
                     </div>
