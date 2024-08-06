9870b3502399264ae071eb4b98651be74fbbf05b
Christian <christdao19@gmail.com>
2024-08-07 07:44:35 +0800

    Remove meta

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,6 @@ use Illuminate\Support\Facades\URL;
 |
 */
 
-if (env('APP_ENV') === 'production') {
-    URL::forceSchema('https');
-}
-
-
 // Home
 Route::get('/', function () {
     return view('welcome');
